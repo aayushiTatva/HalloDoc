48400d4b5c10bf22c69c92ab2690882293936848
aayushiTatva <aayushi.dhruva@etatvasoft.com>
2024-02-28 18:22:41 +0530

    Add project files.

---
+++ Controllers/AdminController/DashboardController.cs
@@ -0,0 +1,81 @@
+﻿using HalloDocMVC.DBEntity.DataContext;
+using HalloDocMVC.DBEntity.ViewModels.AdminPanel;
+using HalloDocMVC.Repositories.Admin.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HalloDocMVC.Controllers.AdminController
+{
+    public class DashboardController : Controller
+    {
+        private readonly HalloDocContext _context;
+        private readonly IAdminDashboard _IAdminDashboard;
+        public DashboardController(HalloDocContext context, IAdminDashboard IAdminDashboard)
+        {
+            _context = context;
+            _IAdminDashboard = IAdminDashboard;
+        }
+        public IActionResult Index()
+        {
+            var countRequest = _IAdminDashboard.CardData();
+            return View("~/Views/AdminPanel/Dashboard/Index.cshtml", countRequest);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> _SearchResult(string Status)
+        {
+            if (Status == null)
+            {
+                Status = "1";
+            }
+
+            List<AdminDashboardList> contacts = _IAdminDashboard.GetRequests(Status);
+            switch (Status)
+            {
+                case "1":
+                    TempData["CurrentStatus"] = "New";
+                    break;
+                case "2":
+                    TempData["CurrentStatus"] = "Pending";
+                    break;
+                case "4,5":
+                    TempData["CurrentStatus"] = "Active";
+                    break;
+                case "6":
+                    TempData["CurrentStatus"] = "Conclude";
+                    break;
+                case "3,7,8":
+                    TempData["CurrentStatus"] = "To Close";
+                    break;
+                case "9":
+                    TempData["CurrentStatus"] = "Unpaid";
+                    break;
+            }
+            switch (Status)
+            {
+                case "1":
+                    return PartialView("~/Views/AdminPanel/Dashboard/_NewRequest.cshtml", contacts);
+                    break;
+                case "2":
+                    return PartialView("~/Views/AdminPanel/Dashboard/_PendingRequest.cshtml", contacts);
+                    break;
+                case "4,5":
+                    return PartialView("~/Views/AdminPanel/Dashboard/_ActiveRequest.cshtml", contacts);
+                    break;
+                case "6":
+                    return PartialView("~/Views/AdminPanel/Dashboard/_ConcludeRequest.cshtml", contacts);
+                    break;
+                case "3,7,8":
+                    return PartialView("~/Views/AdminPanel/Dashboard/_ToCloseRequest.cshtml", contacts);
+                    break;
+                case "9":
+                    return PartialView("~/Views/AdminPanel/Dashboard/_UnpaidRequest.cshtml", contacts);
+                    break;
+            }
+
+      
+            return PartialView("");
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿/* Confirmation Modal */
+var targetModal = new bootstrap.Modal(document.getElementById('targetModal', {}))
+targetModal.show();
+
+var dismissModal = document.getElementById('dismissModal');
+dismissModal.addEventListener("click", () => {
+    targetModal.hide();
+})
+
+/* Phone number */
+
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+/* Choose File */
+$("#files").change(function () {
+    filename = this.files[0].name;
+    console.log(filename);
+    $("#choosenfile").text(filename);
+});
+
+/* Password */
+
+function passtoggle() {
+    var x = document.getElementById("floatingPassword");
+    if (x.type === "password") {
+        x.type = "text";
+        document.querySelectorAll("i.fa.fa-eye-slash").forEach(i => i.style.display = "none");
+        document.querySelectorAll("i.fa.fa-eye").forEach(i => i.style.display = "block");
+    }
+    else {
+        x.type = "password";
+        document.querySelectorAll("i.fa.fa-eye-slash").forEach(i => i.style.display = "block");
+        document.querySelectorAll("i.fa.fa-eye").forEach(i => i.style.display = "none");
+    }
+}

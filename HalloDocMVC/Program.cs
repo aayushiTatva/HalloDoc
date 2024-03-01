8dcf76265cd66caf9ade9b8e1f9791f219df6d71
aayushiTatva <aayushi.dhruva@etatvasoft.com>
2024-03-01 17:13:34 +0530

    program.cs updated

--- HalloDocMVC/Program.cs
+++ HalloDocMVC/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreHero.ToastNotification;
 using HalloDocMVC.DBEntity.DataContext;
 using HalloDocMVC.Repositories.Admin.Repository;
 using HalloDocMVC.Repositories.Admin.Repository.Interface;
@@ -8,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<HalloDocContext>();
 builder.Services.AddScoped<IAdminDashboard, AdminDashboard>();
-
+builder.Services.AddScoped<IActions, Actions>();
+builder.Services.AddScoped<IComboBox, ComboBox>();
+builder.Services.AddNotyf(config => { config.DurationInSeconds = 3; config.IsDismissable = true; config.Position = NotyfPosition.TopRight; });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

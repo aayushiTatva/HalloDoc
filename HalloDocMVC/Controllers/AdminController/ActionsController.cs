36454f18b2a021402a9e5930a812f5c41e05881d
aayushiTatva <aayushi.dhruva@etatvasoft.com>
2024-03-01 16:50:44 +0530

    initial commit 2

---
+++ HalloDocMVC/Controllers/AdminController/ActionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDocMVC.Controllers.AdminController
+{
+    public class ActionsController : Controller
+    {
+        public async Task<IActionResult> ViewCase()
+        {
+            return View("~/Views/AdminPanel/Actions/ViewCase.cshtml");
+        }
+        public async Task<IActionResult> ViewNotes()
+        {
+            return View("~/Views/AdminPanel/Actions/ViewNotes.cshtml");
+        }
+    }
+}
